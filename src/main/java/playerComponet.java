4cd0d0026618a7a71cda678f136e8610023a0bab
nimeifu <gliu3637@bths.edu>
2019-06-09 23:15:01 -0400

    d231

---
+++ src/main/java/playerComponet.java
@@ -0,0 +1,32 @@
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.entity.components.PositionComponent;
+
+public class playerComponet extends Component {
+    private PositionComponent position;
+
+    private double speed =0;
+
+    @Override
+    public void onUpdate(double tpf)
+    {
+        speed =tpf*60;
+    }
+
+    public void up()
+    {
+        position.translateY(-2*speed);
+    }
+
+    public void down()
+    {
+        position.translateY(5*speed);
+    }
+
+    public void left(){
+        position.translateX(-2*speed);
+    }
+
+    public void right(){
+        position.translateX(2*speed);
+    }
+}
