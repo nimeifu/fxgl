8bfd09245dda39602e67fccfdaca855e9b98e34d
nimeifu <gliu3637@bths.edu>
2019-06-13 00:27:07 -0400

    d231

--- src/main/java/BasicGameApp.java
+++ src/main/java/BasicGameApp.java
@@ -1,6 +1,7 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.entity.Entities;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.InputMapping;
@@ -8,6 +9,7 @@ import com.almasb.fxgl.input.OnUserAction;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.settings.GameSettings;
+
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
@@ -28,10 +30,7 @@ public class BasicGameApp extends  GameApplication {
     private String hitsound;
     private final int ScreenWidth = 1024;
     private final int ScreenHeight = 633;
-    public enum EntityType
-    {
-        player,item
-    }
+
     private playerComponet playerComponet;
 
 
@@ -40,37 +39,23 @@ public class BasicGameApp extends  GameApplication {
         gameSettings.setWidth(ScreenWidth);
         gameSettings.setHeight(ScreenHeight);
         gameSettings.setTitle("basic game");
-        gameSettings.setVersion("Version beta");
-        gameSettings.setMenuEnabled(false);
-        gameSettings.setIntroEnabled(false);
-        gameSettings.setProfilingEnabled(false);
+        gameSettings.setConfigClass(Config.class);
+
     }
 
     @Override
     protected void initGame() {
-        super.initGame();
-        player = Entities.builder()
-                .type(EntityType.player)
-                .at(300, 300)
-                .viewFromNode(new Rectangle(25, 25, Color.BLUE))
-                .with(new CollidableComponent(true))
-                .buildAndAttach(getGameWorld());
+       getGameWorld().addEntityFactory(new Factory());
+       Entity player =getGameWorld().spawn("Player",ScreenWidth/2,ScreenHeight/2);
+       //Entity princess=getGameWorld().spawn("princess",800,ScreenHeight/2);
+       playerComponet=player.getComponent(playerComponet.class);
+       getGameWorld().spawn("background");
 
-        NPC=    Entities.builder()
-                .at(200,200)
-                .with(new CollidableComponent(true))
-                .buildAndAttach(getGameWorld());
-
-        Entities.builder()
-                .type(EntityType.item)
-                .at(150,150)
-                .with(new CollidableComponent(true))
-                .buildAndAttach(getGameWorld());
 
     }
-
+/*
     @Override
-    protected void initPhysics() {
+   protected void initPhysics() {
         getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.player,EntityType.item) {
             @Override
             protected void onCollisionBegin(Entity a, Entity b) {
@@ -80,55 +65,34 @@ public class BasicGameApp extends  GameApplication {
 
 
     }
-
+*/
     @Override
     protected void initInput() {
-        Input input = getInput();
+        getInput().addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                playerComponet.up();
+            }
+        },KeyCode.W);
 
-        input.addInputMapping(new InputMapping("Move Left", KeyCode.A));
-        input.addInputMapping(new InputMapping("Move Right", KeyCode.D));
-        input.addInputMapping(new InputMapping("Move up", KeyCode.W));
-        input.addInputMapping(new InputMapping("Move down", KeyCode.S));
-        input.addInputMapping(new InputMapping("Slash", MouseButton.PRIMARY));
-        input.addInputMapping(new InputMapping("shoot", MouseButton.SECONDARY));
+        getInput().addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                playerComponet.up();
+            }
+        },KeyCode.S);
+
     }
-        @OnUserAction(name = "Move Left")
-        public void moveleft()
-        {
-            playerComponet.left();
-        }
-
-        @OnUserAction(name = "Move Right")
-        public void moveRight()
-        {
-            playerComponet.right();
-        }
-
-        @OnUserAction(name = "Move up")
-        public void moveUp()
-        {
-            playerComponet.up();
-        }
-
-        @OnUserAction(name = "Move down")
-        public void moveDown()
-        {
-            playerComponet.down();
-        }
-
     @Override
     protected void initUI() {
-        Text textPixels = new Text();
-        textPixels.textProperty().bind(getGameState().intProperty("pixelsMoved").asString());
-        textPixels.setTranslateX(50); // x = 50
-        textPixels.setTranslateY(100); // y = 100
 
-        getGameScene().addUINode(textPixels); // add to the scene graph
     }
 
     @Override
     protected void initGameVars(Map<String, Object> vars) {
-        vars.put("pixelsMoved", 0);
+        vars.put("bullets",10);
+        vars.put("lives",100);
+
     }
 
 
--- src/main/java/playerComponet.java
+++ src/main/java/playerComponet.java
@@ -1,32 +1,80 @@
+import com.almasb.fxgl.app.FXGL;
 import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.entity.components.PositionComponent;
+import com.almasb.fxgl.time.LocalTimer;
+import javafx.util.Duration;
+
+import static com.almasb.fxgl.app.DSLKt.geti;
 
 public class playerComponet extends Component {
     private PositionComponent position;
 
-    private double speed =0;
+    private double speed =1;
+    private LocalTimer fireTimer=FXGL.newLocalTimer();
+    private boolean canFire=true;
+    private LocalTimer shootTimer= FXGL.newLocalTimer();
+    private boolean canShoot=true;
+    //private WeaponType weapon=WeaponType.slash;
 
     @Override
     public void onUpdate(double tpf)
     {
+
         speed =tpf*60;
+        //position.translateX(tpf*FXGL.<Config>getGameConfig().getPlayerSpeed());
+
+        if(fireTimer.elapsed(Duration.seconds(0.10))){
+            canFire=true;
+        }
+        if(shootTimer.elapsed(Duration.minutes(0.30)))
+        {
+            canShoot=true;
+        }
     }
 
     public void up()
     {
-        position.translateY(-2*speed);
+        if(entity.getY()<100)
+        {
+            position.translateY(- speed);
+        }
+        return;
     }
 
     public void down()
     {
-        position.translateY(2*speed);
+        if(entity.getY()<633-speed)
+        {
+            position.translateY(speed);
+        }
+        return;
     }
 
     public void left(){
-        position.translateX(-2*speed);
+        if(entity.getX()>speed)
+        {
+            position.translateX(-speed);
+        }
+        return;
     }
 
     public void right(){
-        position.translateX(2*speed);
+        if(entity.getX()<1024+speed){
+            position.translateX(speed);
+        }
+        return;
+    }
+
+    public void fire(){
+        if(geti("bullets")==0)
+        {
+            return;
+        }
+        if(!canFire)
+        {
+            return;
+        }
+        fireTimer.capture();
+        canFire=false;
     }
 }
